5ba9364a4be339e707cfe1019ed6d3ae45fba5e0
orsoh <orsoh@mail.ru>
2022-04-09 23:12:08 +0300

    stud

--- HW_2/tsiahniy_HW_2.py
+++ HW_2/tsiahniy_HW_2.py
@@ -6,35 +6,62 @@
 #- якщо вік користувача складається з однакових цифр (11, 22, 44 і тд років) - вивести "Який цікавий вік!"
 #- у будь-якому іншому випадку - вивести "А білетів вже немає!"
 
-age = input('Укажите свой полный возраст')
-age = age.replace(' ', '')
-age = age.lstrip('0')
+client_age = ''
+while not client_age.isdigit():
+    client_age = input('Укажите свой полный возраст ---> ').replace(' ', '')
+    if len(client_age) > 1:
+        client_age.lstrip('0')
+    if not client_age.isdigit():
+        print('Не верный ввод данных')
+    elif int(client_age) > 130:
+        client_age = ''
+        print('Не верный возраст')
 
-if age.isdigit() == True:
-    age = int(age)
-else:
-    print('Не верный возраст')
-    exit()
+age_cool = client_age.count(client_age[0]) == len(client_age)
+if len(client_age) == 1:
+    age_cool = not age_cool
 
-age_str = str(age)
-counter = len(age_str) - 1
-if counter > 0:
-    while counter > 0:
-        if int(age_str[counter]) == int(age_str[counter-1]): #последовательно сравниваю
-            counter -= 1                                     #значения от последнего до нулевого индекса
-        else:
-            break
-    if counter == 0:
-         print('Какой интересный возраст!')
-
-if age <= 7:
+client_age_int = int(client_age)
+if client_age_int < 7:
     print('Где твои родители?')
-elif 7 < age <= 16:
+elif client_age_int < 16 and not age_cool:
     print('Это фильм для взрослых')
-elif age >= 65 :
+elif client_age_int > 65 and not age_cool:
     print('Покажите пенсионное удостоверение')
+elif age_cool:
+    print('Какой интересный возраст!')
 else:
     print('А билетов уже нет!')
+
+
+#СТАРЫЙ КОД
+#    age = input('Укажите свой полный возраст')
+#    age = age.replace(' ', '')
+#    age = age.lstrip('0')
+#
+#    if age.isdigit() == True:
+#        age = int(age)
+#    else:
+#        print('Не верный возраст')
+#        exit()
+#
+#    age_str = str(age)
+#    counter = len(age_str) - 1
+#    if counter > 0:
+#        while counter > 0:
+#            if int(age_str[counter]) == int(age_str[counter - 1]):  # последовательно сравниваю
+#                counter -= 1  # значения от последнего до нулевого индекса
+#            else:
+#                break
+#        if counter == 0:
+#            print('Какой интересный возраст!')
+#
+#    if age <= 7:
+#        print('Где твои родители?')
+#    elif 7 < age <= 16:
+#       print('Покажите пенсионное удостоверение')
+#    else:
+#        print('А билетов уже нет!')
 
 
 
