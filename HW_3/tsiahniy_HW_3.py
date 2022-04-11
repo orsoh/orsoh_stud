d9ae68606670e2ea2594cc09a49c6ffb9ae8c8ec
orsoh <103059317+orsoh@users.noreply.github.com>
2022-04-12 01:23:31 +0300

    Update tsiahniy_HW_3.py

--- HW_3/tsiahniy_HW_3.py
+++ HW_3/tsiahniy_HW_3.py
@@ -20,10 +20,24 @@ while not new_word.isalpha() or not len(new_word) > 0:
     if not new_word.isalpha():
         print('Данные не являются словом')
 
-while not new_number.isdigit() or str(new_number) > len(new_word):
-    new_number = input('Введите число ---> ').replace(' ', '')
+   
+print(f'Вы ввели слово {new_word}')
 
-    if not new_number.new_number():
+
+while not new_number.isdigit():
+    new_number = input(f'Введите число 1 до {len(new_word)} ---> ').replace(' ', '')
+    
+    if not new_number.isdigit():
         print('Данные не являются числом')
-    elif str(new_number) > len(new_word):
+        new_number = ''
+    elif int(new_number) > len(new_word):
         print('Число слишком большое')
+        new_number = ''
+    elif int(new_number)==0:
+        print('Число не должно быть равно нулю')
+        new_number = ''
+
+   
+print(f'Вы ввели число {new_number}')
+
+print(f'В слове {new_word} символ под номером {new_number} это {new_word[int(new_number)-1]}')
