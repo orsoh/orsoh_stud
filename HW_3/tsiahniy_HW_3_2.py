401fb045e64e79986dd3cd666644a39d291e50c1
orsoh <103059317+orsoh@users.noreply.github.com>
2022-04-12 04:07:04 +0300

    Update tsiahniy_HW_3_2.py

--- HW_3/tsiahniy_HW_3_2.py
+++ HW_3/tsiahniy_HW_3_2.py
@@ -1,1 +1,20 @@
+# 2.Вести з консолі строку зі слів (або скористайтеся константою).
+# Напишіть код, який визначить кількість кількість слів, в цмх даних.
+status_line = False
 
+while status_line == False:
+    new_line = input('Введите строку ---<  ').strip().split(' ')
+    
+    counter = new_line.count('')
+
+    for i in range(counter):
+        new_line.remove('')
+        
+    status_line = True
+    for word in new_line:
+        if not word.isalpha():
+            print(f'{word} is not word')
+            status_line = False
+        
+            
+print(new_line)
