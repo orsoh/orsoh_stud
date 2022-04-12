e40aeb4cb78331ca7e955d4ade5c2a3b8ceeb967
orsoh <orsoh@mail.ru>
2022-04-12 22:47:00 +0300

    stud

--- HW_3/tsiahniy_HW_3_2.py
+++ HW_3/tsiahniy_HW_3_2.py
@@ -1,27 +1,71 @@
 # 2.Вести з консолі строку зі слів (або скористайтеся константою).
 # Напишіть код, який визначить кількість кількість слів, в цмх даних.
-status_line = False
 
-while status_line == False:
-    status_line = True
-    new_line = input('Введите строку ---<  ').strip().split(' ')
+
+new_line = input('Введите строку ---<  ').strip().replace("\n", " ").split(' ')
+
+line_exception0 = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '№', '#')
+for element0 in line_exception0:
+    counter = 0
+    while not counter > len(new_line) - 1:
+        if len(new_line[counter]) == 0:
+            counter += 1
+            continue
+        elif new_line[counter].find(element0) == 0:
+            new_line[counter] = new_line[counter][1:]
+            continue
+        else:
+            counter += 1
+
+line_exception1 = ('!', '\"', '.', ',', '?', ':', ';', '\'', '-', '(', ')', '[', ']', '#', '*', '@', '«', '»')
+
+for element1 in line_exception1:
+    counter = 0
+    while not counter > len(new_line) - 1:
+        if len(new_line[counter]) == 0:
+            counter += 1
+            continue
+        elif new_line[counter].rfind(element1) == (len(new_line[counter]) - 1):
+            new_line[counter] = new_line[counter][:-1]
+            continue
+        elif new_line[counter].find(element1) == 0:
+            new_line[counter] = new_line[counter][1:]
+            continue
+        else:
+            counter += 1
+
+line_exception2 = ('\'', '-')
+for element2 in line_exception2:
+    counter = 0
+    while not counter > len(new_line) - 1:
+        if len(new_line[counter]) == 0:
+            counter += 1
+            continue
+        elif new_line[counter].find(element2) != -1:
+            slice_index = new_line[counter].find(element2)
+            new_line[counter] = new_line[counter][:slice_index] + new_line[counter][slice_index + 1:]
+            continue
+        else:
+            counter += 1
+
+
+counter = new_line.count('')
     
-    counter = new_line.count('')
-    
-    for i in range(counter):
-        try:
-            new_line.remove('')
-        except Exception as error_list:
-            print('Я сломался')
-            print(error_list)
-            status_line = False
-            
-    for word in new_line:
-        if not word.isalpha():
-            print(f'{word} не является словом')
-            status_line = False
-        
+for i in range(counter):
+    try:
+        new_line.remove('')
+    except Exception as error_list:
+        print('Я сломался')
+        print(error_list)
 
-counter_word = len(new_line)
 
-print(f'Колличество слов: {counter_word}')
+counter_words = 0
+for word in new_line:
+    if not word.isalpha():
+        print(f'{word} не является словом')
+    else:
+        counter_words += 1
+
+
+
+print(f'Колличество слов: {counter_words}')
--- svalka/svalka.py
+++ svalka/svalka.py
@@ -1,41 +1,36 @@
-client_age = ''
-while not client_age.isdigit():
-    client_age = input('Укажите свой полный возраст ---> ').replace(' ', '')
-    if len(client_age) > 1:
-        client_age.lstrip('0')
-    if not client_age.isdigit():
-        print('Не верный ввод данных')
-    elif int(client_age) > 130:
-        client_age = ''
-        print('Не верный возраст')
-client_age_int = int(client_age)
-massiv = [11, 22, 33, 44, 55, 66, 77, 88, 99, 111]
-counter = 0
-
-while not counter == 10:
-    try:
-        k = 1 / (client_age_int - massiv[counter])
-        counter = counter +1
-    except ZeroDivisionError:
-        k = 0
-        print('Какой интересный возраст!')
-        counter = 10
-        exit()
-
-print('Кyjhv')
 
 
-new_line = ['sda', '', 'sad!!']
-exception = ('!', '\'', '\"', '.', ',', '?', ':', ';')
-for element in exception:
+
+new_line = ['--s!!---da???', '№№№--!!--!!', '!s--ad!!']
+line_exception1 = ('!',  '\"', '.', ',', '?', ':', ';', '\'', '-', '(', ')', '[', ']', '#', '№')
+for element1 in line_exception1:
     counter = 0
-    while not counter == len(new_line):
-        if new_line[counter].find(element) == len(new_line[counter]-1):
-            new_line[counter].remove[len(new_line[counter])-1]
+    while not counter > len(new_line) - 1:
+        if len(new_line[counter]) == 0:
+            counter += 1
             continue
-        elif new_line[counter].find(element) == 0:
-            new_line[counter].remove[0]
+        elif new_line[counter].rfind(element1) == (len(new_line[counter]) - 1):
+            new_line[counter] = new_line[counter][:-1]
+            continue
+        elif new_line[counter].find(element1) == 0:
+            new_line[counter] = new_line[counter][1:]
             continue
         else:
             counter += 1
+
+
+line_exception2 = ('\'', '-')
+for element2 in line_exception2:
+    counter = 0
+    while not counter > len(new_line) - 1:
+        if len(new_line[counter]) == 0:
+            counter += 1
+            continue
+        elif new_line[counter].find(element2) != -1:
+            slice_index = new_line[counter].find(element2)
+            new_line[counter] = new_line[counter][:slice_index] + new_line[counter][slice_index+1:]
+            continue
+        else:
+            counter += 1
+
 print(new_line)          
