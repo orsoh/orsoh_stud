0943fdd6484605b8c62a1d45ce7d89825130bd1b
orsoh <orsoh@mail.ru>
2022-05-02 10:11:23 +0300

    d

--- HW_7/tsiahniy_HW_7.py
+++ HW_7/tsiahniy_HW_7.py
@@ -1,9 +1,35 @@
+# Hапишіть программу "Касир в кінотеатрі", яка буде виконувати наступне:
+#
+# Попросіть користувача ввести свсвій вік.
+# - якщо користувачу менше 7 - вивести "Тобі ж <>! Де твої батьки?"
+# - якщо користувачу менше 16 - вивести "Тобі лише <>, а це е фільм для дорослих!"
+# - якщо користувачу більше 65 - вивести "Вам <>? Покажіть пенсійне посвідчення!"
+# - якщо вік користувача складається з однакових цифр (11, 22, 44 і тд років, всі можливі варіанти!) - вивести
+#   "О, вам <>! Який цікавий вік!"
+# - у будь-якому іншому випадку - вивести "Незважаючи на те, що вам <>, білетів всеодно нема!"
+#
+# Замість <> в кожну відповідь підставте значення віку (цифру) та правильну форму слова рік
+# Для будь-якої відповіді форма слова "рік" має відповідати значенню віку користувача.
+# Наприклад :
+# "Тобі ж 5 років! Де твої батьки?"
+# "Вам 81 рік? Покажіть пенсійне посвідчення!"
+# "О, вам 33 роки! Який цікавий вік!"
+#
+# Зробіть все за допомогою функцій! Для кожної функції пропишіть докстрінг.
+# Не забувайте що кожна функція має виконувати тільки одну завдання і про правила написання коду.
+
 def input_fun():
+    """
+    Функция запрашивает у клиента данные о возрасте, проводит валидацию данный и возвращает кортеж
+    из данных в формате (str, int)
+
+    :return: кортеж из данных в формате (str, int)
+    """
     while True:
         client_age = input('Укажите свой полный возраст ---> ').replace(' ', '').lstrip('0')
         try:
             client_age_int = int(client_age)
-            if client_age_int > 130:
+            if client_age_int > 130 or client_age_int < 0:
                 print('Не верный возраст')
                 continue
             break
@@ -14,6 +40,14 @@ def input_fun():
 
 def msg_part_fun(client_age, client_age_int):
     """
+    Функция принимает данные о возрасте клиента и в зависимости от принадлежности возрастному
+    отрезку возвращает нужную заготовку сообщения
+
+    :param client_age: данные о возрасте в формате str
+    :type client_age: str
+    :param client_age_int: данные о возрасте в формате int
+    :type client_age_int: int
+    :return msg_part: заготовка сообщения в формате str
     """
     age_cool = client_age.count(client_age[0]) == len(client_age)
     if len(client_age) == 1:
@@ -32,19 +66,40 @@ def msg_part_fun(client_age, client_age_int):
 
 
 def age_part_fun(client_age, client_age_int):
+    """
+    Функция принимает данные о возрасте и после анализа возвращает часть сообщения
+    содержащую возраст клиента и слово "год" в нужной форме
+
+    :param client_age: данные о возрасте в формате str
+    :type client_age: str
+    :param client_age_int: данные о возрасте в формате int
+    :type client_age_int: int
+    :return age_part: часть сообщения с возрастом клиента в формате str
+    """
     if 9 < client_age_int < 20 or 109 < client_age_int < 120:
-        age = f'{client_age_int} лет'
+        age_part = f'{client_age_int} лет'
     else:
         if 4 < int(client_age[-1]) < 10 or int(client_age[-1]) == 0:
-            age = f'{client_age} лет'
+            age_part = f'{client_age} лет'
         elif 1 < int(client_age[-1]) < 5:
-            age = f'{client_age} года'
+            age_part = f'{client_age} года'
         else:
-            age = f'{client_age} год'
-    return age
+            age_part = f'{client_age} год'
+    return age_part
 
-client_age_tpl = input_fun()
-msg = msg_part_fun(client_age=client_age_tpl[0], client_age_int=client_age_tpl[1])
-age = age_part_fun(client_age=client_age_tpl[0], client_age_int=client_age_tpl[1])
-res_str = msg.format(age_part=age)
-print(res_str)
+
+def start_all():
+    """
+    Функция, вызов которой запускает выполнения всего кода
+    Всё работает и без неё,
+    но "Зробіть все за допомогою функцій! Для кожної функції пропишіть докстрінг."
+    :return:
+    """
+    client_age_tpl = input_fun()
+    msg = msg_part_fun(client_age=client_age_tpl[0], client_age_int=client_age_tpl[1])
+    age = age_part_fun(client_age=client_age_tpl[0], client_age_int=client_age_tpl[1])
+    res_str = msg.format(age_part=age)
+    print(res_str)
+
+
+start_all()
